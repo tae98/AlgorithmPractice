5ae2e74319db13c531609abdc4dc829a8e428567
tae98 <72403590+tae98@users.noreply.github.com>
2024-08-12 14:25:43 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        int a = num1;
+        int b = num2;
+        if (a == b){
+            answer = 1;
+        }else{
+            answer =-1;
+        }
+        return answer;
+    }
+}
