664bb29d98ec0e9429af78adb505314fa47c4836
tae98 <72403590+tae98@users.noreply.github.com>
2024-08-13 09:28:41 +0900

    [level 1] Title: 덧칠하기, Time: 1.87 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int painted = 0;
+
+        for(int each: section){
+            if(each > painted){
+                painted = each + m -1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
