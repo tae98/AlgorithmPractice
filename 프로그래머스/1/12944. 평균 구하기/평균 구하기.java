3b891b17ee03fd71aacf4024b2816da3ed302d1f
tae98 <72403590+tae98@users.noreply.github.com>
2024-08-16 20:48:33 +0900

    [level 1] Title: 평균 구하기, Time: 0.07 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double total = 0;
+        for(int num:arr){
+            total +=  num;
+        }
+        answer = total / arr.length;
+        
+        return answer;
+    }
+}
