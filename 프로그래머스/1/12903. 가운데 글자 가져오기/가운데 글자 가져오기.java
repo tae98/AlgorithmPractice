36be0c87fd36c40ea6f8d8ceedfe0bd7dd3dc388
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-09-01 21:45:48 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int mid = s.length()/2;
+        
+        if(s.length() % 2 == 0){
+            answer = s.substring(mid-1,mid+1); 
+        }else{answer = s.substring(mid,mid+1);}
+        return answer;
+    }
+}
