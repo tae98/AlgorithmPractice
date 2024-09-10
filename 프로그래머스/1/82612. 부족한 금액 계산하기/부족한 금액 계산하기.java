0e06dded67595faa99eb5213bede090148e8a02d
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-09-10 17:35:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.52 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        
+        long answer = 0;
+        long total =0;
+        
+        for(int i = 1; i <= count; i++){
+            total += i * price;
+        }
+        
+        if(total> money){
+            answer = total - money;
+        }
+        
+
+        return answer;
+    }
+}
