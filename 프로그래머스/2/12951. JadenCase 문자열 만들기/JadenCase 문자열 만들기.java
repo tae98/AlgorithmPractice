0f7b0292be71c91bc1bb76afec145f52a27559f0
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-10-07 01:16:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 26.66 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] tmp = s.split(" ");
+        
+        for(int i = 0; i < tmp.length; i++){
+            
+            if(tmp[i].length() == 0){
+                answer+= " ";
+            }
+            else{
+            answer += tmp[i].substring(0,1).toUpperCase() + tmp[i].substring(1).toLowerCase() + " ";
+            }
+
+        }
+        if(s.substring(s.length()-1, s.length()).equals(" ")) return answer;
+        return answer.substring(0, answer.length()-1);
+    }
+}
