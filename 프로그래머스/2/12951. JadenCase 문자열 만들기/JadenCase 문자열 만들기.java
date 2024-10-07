4c53e559cb3f1e66692c8a844cbcc6edacc0acfd
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-10-07 20:18:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 22.79 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -4,16 +4,18 @@ class Solution {
         String[] tmp = s.split(" ");
         
         for(int i = 0; i < tmp.length; i++){
-            
+            // 문자열의 길이가 0이라면 공백 추가         
             if(tmp[i].length() == 0){
                 answer+= " ";
             }
             else{
-            answer += tmp[i].substring(0,1).toUpperCase() + tmp[i].substring(1).toLowerCase() + " ";
+                answer += tmp[i].substring(0,1).toUpperCase()+ tmp[i].substring(1).toLowerCase()+" ";
             }
-
         }
+        // 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
         if(s.substring(s.length()-1, s.length()).equals(" ")) return answer;
+        // 맨 마지막 " " 제거하고 answer 반환
         return answer.substring(0, answer.length()-1);
     }
 }
+        
