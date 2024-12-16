072cc444bda2761b7202dc4b22411b2a078ace31
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-16 16:30:26 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        for(int i = 0; i < number.length;i++){
+       
+        int x = number.length;
+        for(int i = 0; i < number.length; i++){
             for(int j = i+1; j < number.length; j++){
-                for(int k = j+1; k < number.length; k++){
-                    if(number[i]+number[j]+number[k]==0){
-                        answer+=1;
+                for(int k = j+1 ; k < number.length; k++){
+                    if(number[i]+number[j]+number[k] == 0){
+                        answer++;
                     }
                 }
             }
