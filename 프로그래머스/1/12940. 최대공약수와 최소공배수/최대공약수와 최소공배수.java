19877f41b913ddb99beabb3be3cd6c349814e040
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-16 15:03:48 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int[] answer = {n,m};
-        int min = Math.min(n, m);;
-        int max =Math.max(n,m);
-        
-        
-        
+        int[] answer = {n , m};
+        int min = Math.min(n, m);
+        int max = Math.max(n, m);
         
         while(min != 0){
             int r = max % min;
             max = min;
             min = r;
         }
-
         
         answer[0] = max;
         answer[1] = (n * m) / max;
-    
-        
         return answer;
     }
 }
