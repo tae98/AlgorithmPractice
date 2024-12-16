aab0650235d4f91109d7323b0d52eba19681aa05
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-16 17:31:57 +0900

    [level 1] Title: 최소직사각형, Time: 1.71 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,16 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int max_v =0;
-        int max_h =0;
+        int maxV=0;
+        int maxH=0;
         
-        for(int i = 0; i<sizes.length;i++){
+        for(int i =0; i < sizes.length;i++){
             int v = Math.max(sizes[i][0], sizes[i][1]);
             int h = Math.min(sizes[i][0], sizes[i][1]);
-            max_v = Math.max(max_v, v);
-            max_h = Math.max(max_h, h);
+            maxV = Math.max(maxV, v);
+            maxH = Math.max(maxH, h);
         }
-        answer = max_v*max_h;
+        answer = maxV *maxH;
         return answer;
     }
 }
