2c194e9c708265ccc0fd868de774c875b6ad0cbe
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-16 16:15:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.98 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,30 +1,21 @@
-import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        //리스트로 만들어서 향산된 for문으로 단어식이동
-        // 이중for문으로 그단어의 length()의따라 uppercase
         String[] strlst = s.split("");
-        // String[] upperlst = upper.split(" ");
+        
         int counter = 0;
-        
-        
-        
         for(int i =0; i<strlst.length; i++){
             if(strlst[i].equals(" ")){
-                counter = 0;
-                
-            }else if(counter%2 == 0){    
+                counter =0;
+            }else if(counter % 2 == 0){
                 strlst[i] = strlst[i].toUpperCase();
-                counter ++;
-            
-            }else if (counter%2 != 0){
+                counter++;
+            }else if(counter % 2 != 0){
                 strlst[i] = strlst[i].toLowerCase();
-                counter ++;
+                counter++;
             }
             answer += strlst[i];
         }
-
         return answer;
     }
 }
