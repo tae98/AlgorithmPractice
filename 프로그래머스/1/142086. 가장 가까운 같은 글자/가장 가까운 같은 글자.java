f235af026a8697147ea0a49378a0d16efc1d589b
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-17 16:43:16 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 7.95 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,16 +3,13 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         Stack<Character> tmp = new Stack<>();
-        List<Integer> lst = new ArrayList<>();
         
         for(int i =0; i < s.length();i++){
-                lst.add(tmp.search(s.charAt(i)));
+                answer[i] = tmp.search(s.charAt(i));
                 tmp.push(s.charAt(i));
         }
         
-        for(int i =0; i< lst.size();i++){
-            answer[i] = lst.get(i);
-        }
+      
         return answer;
     }
 }
