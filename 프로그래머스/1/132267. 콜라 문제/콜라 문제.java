228d48263fc30fe57261f9e92ac893759e1cf594
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-18 15:16:51 +0900

    [level 1] Title: 콜라 문제, Time: 25.48 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,24 @@
+import java.util.*;
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int store = 0;
-
+        int tmp = 0;
+        //n = 처음 콜라병 a = 교환에필요한 콜라병 b = 교환해주는콜라병
         while(n >= a){
-            store = n % a;
-            n = b * (n/a);
-            answer += n;
-            n += store;
-        }
-         
+            if(n % a == 0){
+                n = (n/a) * b;
+                System.out.println("n % 2 : " + n);
+                answer += n;
+            }else{
+                tmp = n % a;
+                n = ((n/a) * b);
+                
+                answer += n;
+                n = n + tmp;
+                System.out.println("n !% 2 : " + n);
+                System.out.println("tmp : " + tmp);
+            }
+         } //20 6(2) 2(2) 1(1)
         return answer;
     }
 }
