ba66dc7954137da9c4fd9b00e2f836227852a17d
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-18 16:45:42 +0900

    [level 1] Title: 카드 뭉치, Time: 0.54 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,45 +1,27 @@
 import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        Stack<String> card1Stack = new Stack<>();
-        Stack<String> card2Stack = new Stack<>();
-        boolean flag = true;
-        
-        //카드1 스택화
-        for(int i = cards1.length -1; i>= 0; i--){
-            card1Stack.push(cards1[i]);
+        String answer = "Yes";
+        Stack<String> stack1 = new Stack<>();
+        Stack<String> stack2 = new Stack<>();
+        for(int i = cards1.length - 1; i >= 0; i--){
+            stack1.push(cards1[i]);
+        }
+        for(int i = cards2.length - 1; i >= 0; i--){
+            stack2.push(cards2[i]);
         }
         
-        //카드2 스택화
-         for(int i = cards2.length -1; i>= 0; i--){
-            card2Stack.push(cards2[i]);
+        for(int i = 0; i < goal.length; i++){
+            if(!stack1.isEmpty() && goal[i].equals(stack1.peek())){
+                stack1.pop();
+            }else if(!stack2.isEmpty() && goal[i].equals(stack2.peek())){
+                System.out.print(goal[i]);
+                stack2.pop();
+            }else{
+                answer ="No";
+            }
         }
         
-
-        
-        for(int i = 0; i <goal.length; i++){
-            //카드1 스택마지막이 goal의 단어랑 일치하고 스택이 비어있지않을시
-            if(!card1Stack.isEmpty() && (goal[i].equals(card1Stack.peek()))){
-                     card1Stack.pop();
-                     continue;
-                
-            }//카드2 스택마지막이 goal의 단어랑 일치하고 스택이 비어있지않을시
-            else if(!card2Stack.isEmpty() && goal[i].equals(card2Stack.peek())){
-                    card2Stack.pop();
-                    continue;}       
-            else{
-                flag = false;
-                break;}
-       }
-        if(flag == true){
-         answer = "Yes";
-        }else{
-         answer ="No";
-     }
-        
-     return answer;
-
+        return answer;
+    }
 }
-}
-
