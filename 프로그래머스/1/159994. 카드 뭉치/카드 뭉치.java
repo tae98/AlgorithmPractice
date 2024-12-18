f11d8c19033f7e7ecb76d625a4d3da90c250789e
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-18 16:47:25 +0900

    [level 1] Title: 카드 뭉치, Time: 0.22 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -4,6 +4,7 @@ class Solution {
         String answer = "Yes";
         Stack<String> stack1 = new Stack<>();
         Stack<String> stack2 = new Stack<>();
+        
         for(int i = cards1.length - 1; i >= 0; i--){
             stack1.push(cards1[i]);
         }
@@ -15,7 +16,6 @@ class Solution {
             if(!stack1.isEmpty() && goal[i].equals(stack1.peek())){
                 stack1.pop();
             }else if(!stack2.isEmpty() && goal[i].equals(stack2.peek())){
-                System.out.print(goal[i]);
                 stack2.pop();
             }else{
                 answer ="No";
