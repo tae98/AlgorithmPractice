08f0ae914264a07381d93570321a8c9cd8e40ac5
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-19 23:54:03 +0900

    [level 1] Title: 덧칠하기, Time: 1.85 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -3,7 +3,6 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         int painted = 0;
-
         for(int each: section){
             if(each > painted){
                 painted = each + m -1;
