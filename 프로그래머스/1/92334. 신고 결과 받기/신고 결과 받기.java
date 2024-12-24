edbd59839502e95f784ef57761050901c81c940f
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-12-24 22:21:10 +0900

    [level 1] Title: 신고 결과 받기, Time: 143.79 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,21 +2,21 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-        Map<String, HashSet<String> > map = new HashMap<>();
+        Map<String, HashSet<String> > setmap = new HashMap<>();
         Map<String, Integer> intmap = new HashMap<>();
         
         for(int i = 0; i < id_list.length; i++){
-            map.put(id_list[i], new HashSet<>());
+            setmap.put(id_list[i], new HashSet<>());
             intmap.put(id_list[i],i);
         }
         
         for (int i = 0; i < report.length; i++){
             String[] tmp = report[i].split(" ");
-            map.get(tmp[1]).add(tmp[0]);
+            setmap.get(tmp[1]).add(tmp[0]);
         }
           
         for(int i = 0; i< id_list.length; i++){
-            HashSet<String> set = map.get(id_list[i]);
+            HashSet<String> set = setmap.get(id_list[i]);
             if(set.size()>=k){
                 for(String each: set){
                     int index = intmap.get(each);
