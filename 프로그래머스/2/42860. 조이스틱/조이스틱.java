529512b535f4df85d38ce8de7b06de695fc13ce5
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-03-24 18:17:51 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        int move = len - 1;
+        for(int i =0; i < len; i++){
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            int idx = i + 1;
+            while(idx < len && name.charAt(idx) == 'A'){
+                idx++;
+            }
+            move = Math.min(move, i * 2 + len - idx);
+            move = Math.min(move,(len-idx) * 2 + i);
+        }
+        return answer + move;
+    }
+}
